4aaa508db7b8b1b866b275b348da93b5da2b888f
adameldredge <43678759+adameldredge@users.noreply.github.com>
2022-04-20 16:50:05 -0400

    Add files via upload

--- NEWadjList.h
+++ NEWadjList.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <stack>
 #include <queue>
+#include "WebScraper.h"
 
 using namespace std;
 
@@ -37,6 +38,14 @@ struct AdjacencyList
         {
             return ((d.name == name) && (d.adjNodes == adjNodes) && (d.id == id));
         }
+        void setProfileLink(string l) {
+            int index = l.find("nm") + 2;
+            try {
+                this->id = stoi(l.substr(index, 7));
+                this->profileLink = l;
+            }
+            catch (std::invalid_argument) {}
+        }
         void setID(unsigned int i)
         {
             id = i;
@@ -54,6 +63,7 @@ struct AdjacencyList
             return adjNodes;
         }
         string name;
+        string profileLink;
         unsigned int id;
         bool visited;
         bool identified;
@@ -64,22 +74,22 @@ struct AdjacencyList
     Person* root;
     int size;
     map<string, Person*> nodes;
+    WebScraper w;
 
     // Functions
     AdjacencyList()
     {
         root = nullptr;
         size = 0;
+        loopAddData();
     }
     Person* newPerson(string name)
     {
         Person* p = new Person(name);
-        p->setID(size+1);
         size++;
         if (root == nullptr)
             root = p;
         nodes.emplace(name,p);
-
         return p;
     }
     void addEdge(string srcName, string destName)
@@ -104,8 +114,70 @@ struct AdjacencyList
         if (temp->id == id)
             return temp;
     }
+    void recursiveAddEdges(unsigned int count, string name, bool& endRecursion) {
+        if (count >= 3) {
+            return;
+        }
+        if (size > 110000) {
+            //cout << "110,000 nodes!" << endl;
+            endRecursion = true;
+            return;
+        }
+        vector<string> moviesFromProfile;
+        bool killSwitch = false;
+        w.getProfileMovieLinks(nodes[name]->profileLink, moviesFromProfile);
+        vector<string> castAtIndex;
+        vector<string> profilesFromMovie;
+        for (unsigned int i = 0; i < moviesFromProfile.size(); i++) {
+            castAtIndex = w.getMovieRoster(moviesFromProfile.at(i), profilesFromMovie);
+            for (unsigned int j = 0; j < castAtIndex.size(); j++) {
+                if (killSwitch) {
+                    break;
+                }
+                addEdge(name, castAtIndex.at(j));
+                if (nodes[castAtIndex.at(j)]->profileLink == "") {
+                    nodes[castAtIndex.at(j)]->setProfileLink(profilesFromMovie.at(j));
+                }
+                recursiveAddEdges(count + 1, castAtIndex.at(j), killSwitch);
+            }
+            castAtIndex.clear();
+            if (killSwitch) {
+                break;
+            }
+        }
+        profilesFromMovie.clear();
+        moviesFromProfile.clear();
 
-
+    }
+    void loopAddData() {
+        vector<string> movieLinks; //links to all movies from topThousand at page i
+        vector<string> names; //list of names from the movie at j
+        vector<string> linksToProfiles; //links to actor's profile
+        bool killSwitch = false;
+        for (unsigned int i = 0; i < 11; i++) {
+            movieLinks = w.getLinksFromSearch(w.extractTopThousand(to_string(i + 1)));
+            if (killSwitch) { break; }
+            for (unsigned int j = 0; j < movieLinks.size(); j++) {
+                if (killSwitch) { break; }
+                names = w.getMovieRoster(movieLinks.at(j), linksToProfiles); //now have linkstoprofiles set
+                //we have all our names here, so go through names and add everyone in this movie as well as
+                //everyone in their other movies
+                for (unsigned int k = j + 1; k < names.size(); k++) {
+                    addEdge(names.at(j), names.at(k));
+                }
+                for (unsigned int p = 0; p < names.size(); p++) {
+                    if (nodes[names.at(p)]->profileLink == "") {
+                        nodes[names.at(p)]->profileLink = linksToProfiles.at(p);
+                    }
+                    recursiveAddEdges(0, names.at(p), killSwitch);
+                    if (killSwitch) { break; }
+                }
+                names.clear();
+            }
+            linksToProfiles.clear();
+            movieLinks.clear();
+        }
+    }
 
 
     // Once Graph is fully made
